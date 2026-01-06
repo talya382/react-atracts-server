920abfba6a9edb878961a4ceeb8f62452d1c1ef7
Talya Cohen <talyaacc055@gmail.com>
2026-01-07 01:48:43 +0200

    update server and cors configuration

--- app.js
+++ app.js
@@ -1,4 +1,6 @@
 import express from "express";
+import cors from "cors";
+
 import ordersRoutes from "./routs/orders.js";
 import attractionsRoutes from "./routs/atraction.js";
 import usersRoutes from "./routs/user.js";
@@ -7,19 +9,25 @@ import connectDB from "./config/db.js";
 // יצירת אפליקציית Express
 const app = express();
 
+// 🔹 CORS – חייב להיות לפני ה־routes
+app.use(cors({
+    origin: "http://localhost:3000", // כתובת ה־React
+    methods: ["GET", "POST", "PUT", "DELETE"],
+}));
+
 // middleware לקריאת JSON מהבקשות
 app.use(express.json());
 
 // חיבור למסד הנתונים
 connectDB();
 
-// חיבור כל נתיבי ההזמנות
+// חיבור כל הנתיבים
 app.use("/orders", ordersRoutes);
-app.use("/attractions",attractionsRoutes );
+app.use("/attractions", attractionsRoutes);
 app.use("/users", usersRoutes);
 
+let port = process.env.PORT || 5000;
 
-let port=process.env.PORT || 3000;
 // הרצת השרת
 app.listen(port, () => {
     console.log("Server running on port " + port);
