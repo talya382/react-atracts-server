fe42b47aa5d7b84e117002c12f1be503e7f287f0
talya cohen <talyaacc055@gmail.com>
2026-03-24 02:18:41 +0200

    fix resed

--- services/mailer.js
+++ services/mailer.js
@@ -1,8 +1,10 @@
 import { Resend } from "resend";
-
-const resend = new Resend(process.env.RESEND_API_KEY);
+import dotenv from "dotenv";
+dotenv.config();
 
 export const sendTempPassword = async (toEmail, tempPassword) => {
+  const resend = new Resend(process.env.RESEND_API_KEY);
+  
   await resend.emails.send({
     from: "onboarding@resend.dev",
     to: toEmail,
@@ -10,7 +12,7 @@ export const sendTempPassword = async (toEmail, tempPassword) => {
     html: `
       <div style="font-family: Arial; direction: rtl; padding: 20px;">
         <h2 style="color: #34d399;">🏔️ אטרקציות ישראל</h2>
-        <p>קיבלת סיסמה זמנית להתחברות:</p>
+        <p>קיבלת סיסמה זמנית:</p>
         <h1 style="color: #34d399; letter-spacing: 4px;">${tempPassword}</h1>
         <p>לאחר הכניסה תתבקש לשנות את הסיסמה.</p>
       </div>
